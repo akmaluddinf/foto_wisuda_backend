842af90c93cdc2a878d6e87fae0d4d3c6da7333a
Akmaluddin Fadhilah <akmaluddinfadhilah@gmail.com>
2024-06-07 08:24:42 +0700

    update and fix app ver 2024

--- app.js
+++ app.js
@@ -1,79 +1,55 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
-const port = 3001;
 const cors = require('cors');
-const winston = require('winston');
 
-// Membuat logger Winston
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.printf(({ timestamp, level, message }) => {
-            return `[${timestamp}] ${level}: ${message}`;
-        })
-    ),
-    transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({ filename: 'server.log' }) // Catat log ke berkas
-    ]
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Izinkan hanya alamat URL frontend yang benar untuk mengakses server backend
-const allowedOrigins = ['http://localhost:3000'];
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true, // Izinkan kredensial (cookies, auth headers, dll.)
+const getCurrentTimestamp = () => {
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return timestamp;
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
-// Middleware untuk mengarahkan URL sembarangan
-app.use((req, res, next) => {
-    const validRoutes = ['/download', '/']; // Daftar URL yang valid
-    const requestedRoute = req.path;
-
-    if (!validRoutes.includes(requestedRoute)) {
-        logger.info(`URL: ${requestedRoute} tidak tersedia.`);
-        return res.redirect('/'); // Mengarahkan ke URL root jika URL tidak valid
-    }
-
-    next();
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
-// Mengatur folder untuk file statis
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route untuk mengunduh file .zip
-app.get('/download', (req, res) => {
-    const { filename } = req.query;
+app.post('/download', (req, res) => {
+    const { filename } = req.body;
     const filePath = path.join(__dirname, 'public', 'files', `${filename}.zip`);
 
     if (filename) {
-        const fs = require('fs');
-
         // Cek apakah file ada
         if (fs.existsSync(filePath)) {
             res.download(filePath);
-            logger.info(`File ${filename}.zip berhasil diunduh.`);
+            const infoLog = `${getCurrentTimestamp()} - Download Success for NIM: ${filename}\n`;
+            fs.appendFileSync('logDownloadSuccess.log', infoLog);
         } else {
             res.status(404).json({ error: 'File tidak ditemukan.' });
-            logger.warn(`File ${filename}.zip tidak ditemukan.`);
+            const warnLog = `${getCurrentTimestamp()} - File ${filename}.zip tidak ditemukan.\n`;
+            fs.appendFileSync('server.log', warnLog);
         }
     } else {
         res.status(400).json({ error: 'Parameter filename tidak ditemukan.' });
-        logger.warn('Parameter filename tidak ditemukan dalam permintaan unduh.');
+        const warnLog = `${getCurrentTimestamp()} - Parameter filename tidak ditemukan dalam permintaan unduh.\n`;
+        fs.appendFileSync('server.log', warnLog);
     }
 });
 
-app.listen(port, () => {
-    console.log(`Aplikasi backend berjalan di http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 8001;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
